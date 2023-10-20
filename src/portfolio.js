cbc0851a43924422bb5e5e0bd40cfadd7362afb1
a-sambhab <saisambhav21chaini@gmail.com>
2023-10-20 21:20:36 +0530

    Updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A creative and innovative full stack developer with a passion for building cutting-edge web applications using the latest frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1QBTHTXWLeu1KsqDUQ447cgcWAFCh61cC/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1Ds9ws9fjdiaEEoxwzypPCvFV39CuXXh3/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
